75eafc33692bb88cebe4ce045d96655f27acc582
Amir <alivny1@gmail.com>
2022-11-20 18:51:51 +0200

    created main

---
+++ main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Amir on 20/11/2022.
+//
+
+#include "worldcup23a1.h"
+
+int main() {
+    world_cup_t my_world_cup;
+    cout << my_world_cup.teamsTree.get_tree_height();
+    cout << "hello world_cup_t";
+}
